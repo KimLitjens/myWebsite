7c342dde42b631e6c7a42d694b087e57e2371b01
Kim Litjens <Kimlitjens4@gmail.com>
2020-05-14 12:55:02 +0200

    change mail system

--- dist/contactform.php
+++ dist/contactform.php
@@ -1,22 +1,60 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['mail'];
-$message = $_POST['message'];
 
-$email_from = 'info@kimlitjens.nl';
+// $name = $email = $message = '';
+// $errors = array('email' => '', 'name' => '', 'message' => '');
+// if (isset($_POST['submit'])) {
 
-$email_subject = "New Form Submission";
+//     // check email
+//     if (empty($_POST['email'])) {
+//         $errors['email'] = 'A email is required';
+//     } else {
+//         $email = $_POST['email'];
+//         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//             $errors['email'] = 'email must be a valid email address';
+//         }
+//     }
+//     // check title
+//     if (empty($_POST['name'])) {
+//         $errors['title'] = 'A name is required';
+//     } else {
+//         $name = $_POST['name'];
+//         if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+//             $errors['name'] = 'Name can be letters and spaces only';
+//         }
+//     }
+//     // check message
+//     if (empty($_POST['message'])) {
+//         $errors['message'] = 'A message is required ';
+//     }
+//     //  else {
+//     //     $message = $_POST['message'];
+//     //     if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $message)) {
+//     //         $errors['message'] = 'a message';
+//     //     }
+//     // }
 
-$email_body = "User name: $name.\n" .
-    "User Email: $visitor_email.\n" .
-    "User Message: $message.\n";
+//     if (array_filter($errors)) {
+//         // echo 'errors in the form';
+//     } else {
 
-$to = "itisme@kimlitjens.nl";
+//         $email = mysqli_real_escape_string($_POST['email']);
+//         $name = mysqli_real_escape_string($_POST['name']);
+//         $message = mysqli_real_escape_string($_POST['message']);
 
-$headers = "From: $email_from \r\n";
+//} // end of post check
+if(isset($_POST['submit'])) {
+    $name = mysqli_real_escape_string($_POST['name']);
+    $subject = mysqli_real_escape_string($_POST['subject']);
+    $mailFrom = mysqli_real_escape_string($_POST['email']);
+    $message = mysqli_real_escape_string($_POST['message']);
 
-$headers .= "Reply-To: $visitor_email \r\n";
+    $mailTo = "itisme@kimlitjens.nl";
+    $headers = "From: " . $mailFrom;
+    $txt = "You have received a e-mail from: " . $name. ".\n\n".$message;
 
-mail($to, $email_subject, $email_body, $headers);
 
-header("Location: index.html");
+    mail($mailTo, $subject, $txt, $headers);
+
+    header("Location: index.html"); 
+}
+?> 
